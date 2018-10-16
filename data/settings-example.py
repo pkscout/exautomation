42f0aa80a75e5d12fde5aa46154e1dac72018e6e
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2018-10-15 15:21:54 -1000

    new example settings file

---
+++ data/settings-example.py
@@ -0,0 +1,18 @@
+# ACT CONFIGURATION
+
+
+# CARNEGIE/DARLET CONFIGURATION
+carnegie_user = ''  #required
+carnegie_auth =  '' #required
+
+# COMMONAPP CONFIGURATION
+commonapp_user = '' #required
+commonapp_auth = '' #can be omitted if using RSA keyfile
+
+# SAT CONFIGURATION
+sat_user = '' #required
+sat_auth = '' #required
+
+# FIREWORKS CONFIGURATION
+fireworks_user = '' #required
+fireworks_auth = '' #required
