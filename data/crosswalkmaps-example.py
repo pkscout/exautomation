d6a8ed5626a1b5d3a391fa311c64bf635e393ebf
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2018-11-14 09:27:44 -1000

    added example state abbreviation crosswalk

--- data/crosswalkmaps-example.py
+++ data/crosswalkmaps-example.py
@@ -1,3 +1,5 @@
 crosswalks = {
-               'map1':{'thing1':'another thing', 'thing2':'something different'}
+               'stateabbrevtoname':{'AL':'Alabama', 'AK':'Alaska', 'AZ':'Arizona', 'AR':'Arkansas', 'CA':'California', 'CO':'Colorado', 'CT':'Connecticut', 'DE':'Delaware', 'DC':'District of Columbia',  'FL':'Florida', 'GA':'Georgia', 'HI':'Hawaii', 'ID':'Idaho', 'IL':'Illinois', 'IN':'Indiana', 'IA':'Iowa', 'KS':'Kansas', 'KY':'Kentucky', 'LA':'Louisiana', 'ME':'Maine', 'MD':'Maryland', 'MA':'Massachusetts', 'MI':'Michigan', 'MN':'Minnesota', 'MS':'Mississippi', 'MO':'Missouri', 'MT':'Montana', 'NE':'Nebraska', 'NV':'Nevada', 'NH':'New Hampshire', 'NJ':'New Jersey', 'NM':'New Mexico', 'NY':'New York', 'NC':'North Carolina', 'ND':' North Dakota', 'OH':'Ohio', 'OK':'Oklahoma', 'OR':'Oregon', 'PA':'Pennsylvania', 'RI':'Rhode Island', 'SC':'South Carolina', 'SD':'South Dakota', 'TN':'Tennessee', 'TX':'Texas', 'UT':'Utah', 'VT':'Vermont', 'VA':'Virginia', 'WA':'Washington', 'WV':'West Virginia', 'WI':'Wisconsin', 'WY':'Wyoming', 'AS':'American Samoa', 'GU':'Guam', 'MP':'Northern Mariana Islands', 'PR':'Puerto Rico', 'VI':'U.S. Virgin Islands', 'FM':'Federated States of Micronesia', 'MH':'Marshall Islands', 'PW':'Palau'},
+               'genderfull':{'M':'Male', 'F':'Female'}
+
              }
